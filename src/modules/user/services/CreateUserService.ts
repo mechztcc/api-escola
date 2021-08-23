ad45768247be5a9206a78d4edc18ca8149fc7ab8
mechztcc <dojathan@gmail.com>
2021-08-23 09:30:13 -0300

    Create user / returning token

--- src/modules/user/services/CreateUserService.ts
+++ src/modules/user/services/CreateUserService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { hash } from 'bcrypt';
+import authConfig  from '@config/auth';
+import { sign } from 'jsonwebtoken';
 
 
 interface IRequest {
@@ -11,8 +13,13 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateUserService {
-  public async execute({ name, email, password }: IRequest): Promise<User> {
+  public async execute({ name, email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const emailExists = await usersRepository.findByEmail(email);
@@ -26,7 +33,13 @@ class CreateUserService {
     const user =  usersRepository.create({ name, email, password: hashedPass });
 
     await usersRepository.save(user);
-    return user;
+
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id.toString(),
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+
+    return { user, token };
   }
 }
 
